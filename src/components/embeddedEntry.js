e388947f5651ac1d48b5aac843742b7ff11c6475
KyleShabaga <kylecshabaga@gmail.com>
2019-10-01 18:40:01 -0600

    Fixed embedded entry link to show proper sidebarTOC active.

--- src/components/embeddedEntry.js
+++ src/components/embeddedEntry.js
@@ -1,16 +1,17 @@
 import React from "react"
-import { Card } from 'react-bootstrap'
+import { Card } from "react-bootstrap"
+import { Link } from 'gatsby'
 
-const EmbeddedEntry = ({ title, description, slug, categorySlug}) => {
+const EmbeddedEntry = ({ title, description, slug, categorySlug }) => {
   return (
     <Card className="card-border my-4" style={{ width: "26rem" }}>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
-        <Card.Text className="mb-2 text-muted">{description}
+        <Card.Text className="mb-2 text-muted">{description}</Card.Text>
+        <Card.Text>
+          <Link to={`/doc/${categorySlug}/${slug}`}>View Doc</Link>
         </Card.Text>
-        <Card.Link href={`/doc/${categorySlug}/${slug}`}>View Doc</Card.Link>
-
       </Card.Body>
     </Card>
   )
