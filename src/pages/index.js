ec737aa320421b839b19f0673af397d71969f7bc
KyleShabaga <kylecshabaga@gmail.com>
2019-10-16 16:35:47 -0600

    Auth0

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,8 @@ import Layout from "../layouts/index"
 import SEO from "../components/seo"
 import Home from "../components/HomePage/home"
 import UnderNav from "../components/Undernav/Undernav"
-import { login } from "../utils/auth"
 
 const IndexPage = ({ location }) => {
-  login()
 
   return (
     <Layout location={location}>
